fc52bb392f396a91c3191523f6d13cebe9e2f465
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-03-03 11:45:42 -0500

    init and step 2 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TopNav from './components/TopBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopNav />
     </div>
   );
 }
